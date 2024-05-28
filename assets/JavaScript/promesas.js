3daa10580556b373d8d85495ba5b124513c8d05f
Lverise <diego.pereira.200317@gmail.com>
2024-05-28 13:34:33 -0400

    Tabla
    se agregar una tabla al html y se recuperan los datos de la base de datos

--- assets/JavaScript/promesas.js
+++ assets/JavaScript/promesas.js
@@ -1,5 +1,21 @@
-import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"
+
+
+import { addDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"
 import { db } from "./firebase.js";
 export const registrarPersona = async(persona)=>{
     const docRef = await addDoc(collection(db, "personas"), persona);
 };
+
+export const obtenerPersonas = async()=>{
+    //esto recupera la referencia
+    const ref = collection(db, "personas");
+    //recuperamos una captura de la bd
+    const qSnap = await getDocs(ref);
+    let listado = []
+    qSnap.forEach((doc) => {
+        console.log(doc.id);
+        listado.push({...doc.data(),id:doc.id})
+    });
+    console.log(listado);
+    return listado;
+}
--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,7 +1,8 @@
-import { registrarPersona } from "./promesas.js";
+import { obtenerPersonas, registrarPersona } from "./promesas.js";
 
 addEventListener("load",()=>{
     document.getElementById("btnRegistrar").addEventListener("click", registrar);
+    cargarDatos();
 })
 
 const registrar = ()=>{
@@ -10,17 +11,60 @@ const registrar = ()=>{
     let vNombre = eNombre.value
     let eApellido = document.getElementById("apellido");
     let vApellido = eApellido.value
-    let eTelefono = document.getElementById("telefono");
-    let vTelefono = eTelefono.value
+    let eEdad = document.getElementById("edad");
+    let vEdad = eEdad.value
+    let eRut = document.getElementById("rut");
+    let vRut = eRut.value
     let eCorreo = document.getElementById("correo");
     let vCorreo = eCorreo.value
-    let objeto = {nombre:vNombre,apellido:vApellido,telefono:vTelefono,correo:vCorreo};
+    let eFecha = document.getElementById("fechanaciemiento");
+    let vFecha = eFecha.value
+    let objeto = {nombre:vNombre,apellido:vApellido,edad:vEdad,rut:vRut,correo:vCorreo,fechanacimiento:vFecha};
 
     // console.log(objeto);
 
     registrarPersona(objeto).then(()=>{
         alert("Se registra con exito")
+        cargarDatos();
     }).catch((error)=>{
-        console.log(error);
+        //console.log(error);
+        console.log(doc.data());
     });
+}
+
+const cargarDatos = ()=>{
+    //Traer de las promesas todo lo registrado
+    obtenerPersonas().then((personas)=>{
+        console.log("HOLA")
+        console.log(personas)
+        //Cargarlo en la tabla del html
+        let estructura = ""
+        personas.forEach((p)=>{
+            estructura += "<tr>"
+            estructura += "<td>"+p.nombre+"</td>"
+            estructura += "<td>"+p.apellido+"</td>"
+            estructura += "<td>"+p.rut+"</td>"
+            estructura += "<td>"+p.correo+"</td>"
+            estructura += "<td>"+p.edad+"</td>"
+            estructura += "<td>"+p.fechanacimiento+"</td>"
+            estructura += "<td><button id='UPD"+p.id+"'>Actualizar</button></td>";
+            estructura += "<td><button id='DEL"+p.id+"'>Eliminar</button></td>";
+            estructura += "</tr>";
+        })
+        document.getElementById("cuerpoTabla").innerHTML = estructura;
+        
+        personas.forEach((p)=>{
+            let elemento = document.getElementById("UPD"+p.id);
+            elemento.addEventListener("click",()=>{
+                document.getElementById("UPDnombre").value = p.nombre;
+                document.getElementById("UPDapellido").value = p.apellido;
+                document.getElementById("UPDrut").value = p.rut;
+                document.getElementById("UPDcorreo").value = p.correo;
+                document.getElementById("UPDedad").value = p.edad;
+                document.getElementById("UPDfechanacimiento").value = p.fechanacimiento;
+                document.getElementById("btnActualizar").value = p.id;
+            })
+        })
+    })
+alert("aca estoy")
 }
